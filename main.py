1689e8c41dec648017221d7bba5ef8e7b8051778
bmonagan <bernardmonagan@gmail.com>
2026-04-06 18:33:53 -0400

    refactor main.py to add user prompt argument for content generation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
@@ -6,10 +7,15 @@ load_dotenv()
 api_key = os.getenv('GEMINI_API_KEY')
 if api_key is None:
     raise ValueError("GEMINI_API_KEY is not set in the environment variables.")
+
     
 client = genai.Client(api_key=api_key)
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+content = parser.parse_args()
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    model="gemini-2.5-flash", contents=content)
 
 if not response.usage_metadata:
     print("No usage metadata found in the response.")
