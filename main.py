de28fa015ed44bc0e41d11e2902204102a78d294
bmonagan <bernardmonagan@gmail.com>
2026-04-06 18:32:17 -0400

    remove debug print statement for GEMINI_API_KEY in main.py

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ api_key = os.getenv('GEMINI_API_KEY')
 if api_key is None:
     raise ValueError("GEMINI_API_KEY is not set in the environment variables.")
     
-print(f"GEMINI_API_KEY: {api_key}")
-
-
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
