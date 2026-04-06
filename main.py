5cece5f6409d63353342961bf2f0418d985b7cbf
bmonagan <bernardmonagan@gmail.com>
2026-04-06 18:32:02 -0400

    refactor main.py to improve response handling and formatting

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ print(f"GEMINI_API_KEY: {api_key}")
 
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents ="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
 
+if not response.usage_metadata:
+    print("No usage metadata found in the response.")
+else:
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+print("Response:")
 print(response.text)
