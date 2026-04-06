8803f1fb0e8643ddb956b18b4bb161203194ca43
bmonagan <bernardmonagan@gmail.com>
2026-04-06 18:39:51 -0400

    refactor main.py to correct user prompt argument handling and improve message formatting

--- main.py
+++ main.py
@@ -2,21 +2,25 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.getenv('GEMINI_API_KEY')
 if api_key is None:
     raise ValueError("GEMINI_API_KEY is not set in the environment variables.")
 
-    
+  
 client = genai.Client(api_key=api_key)
 
 parser = argparse.ArgumentParser(description="Chatbot")
-parser.add_argument("_user_prompt", type=str, help="User prompt")
+parser.add_argument("user_prompt", type=str, help="User prompt")
 args = parser.parse_args()
-content = args._user_prompt
+content = args.user_prompt
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents=content)
+    model="gemini-2.5-flash", contents=messages)
+
+
 
 if not response.usage_metadata:
     print("No usage metadata found in the response.")
