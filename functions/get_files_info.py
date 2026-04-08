a47a734963dbee495c2ced21d3cd2c09e7d66d5d
bmonagan <bernardmonagan@gmail.com>
2026-04-07 21:22:52 -0400

    remove debug print statements from get_files_info function

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,12 +4,11 @@ def get_files_info(working_directory, directory="."):
     absolute_working_dir = os.path.abspath(working_directory)
     absolute_directory = os.path.abspath(os.path.join(absolute_working_dir, directory))
     target_dir = os.path.normpath(os.path.join(absolute_working_dir, directory))
-    print(f"Working Directory: {absolute_working_dir}")
-    print(f"Directory: {absolute_directory}")
-    print(f"Target Directory: {target_dir}")
     valid_target_dir = os.path.commonpath([absolute_working_dir, target_dir]) == absolute_working_dir
+    
     if not valid_target_dir:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
     if not os.path.isdir(target_dir):
         return f'Error: "{directory}" is not a valid directory'
 
