6a0d78c94869b842273804d992d0914b08c36f04
bmonagan <bernardmonagan@gmail.com>
2026-04-08 19:44:12 -0400

    Added another test for main in the calculator.

--- test_get_file_content.py
+++ test_get_file_content.py
@@ -4,6 +4,8 @@ print(get_file_content("calculator", "lorem.txt"))
 
 print(get_file_content("calculator", "pkg/calculator.py"))
 
+print(get_file_content("calculator", "main.py"))
+
 print(get_file_content("calculator", "/bin/cat"))
 
 print(get_file_content("calculator", "pkg/does_not_exist.py"))
