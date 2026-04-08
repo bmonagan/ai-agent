284c179d347c9495cbe2d8035332bfd1733eab70
bmonagan <bernardmonagan@gmail.com>
2026-04-07 21:22:36 -0400

    add validation for target directory in get_files_info function

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 def get_files_info(working_directory, directory="."):
     absolute_working_dir = os.path.abspath(working_directory)
     absolute_directory = os.path.abspath(os.path.join(absolute_working_dir, directory))
@@ -9,5 +10,7 @@ def get_files_info(working_directory, directory="."):
     valid_target_dir = os.path.commonpath([absolute_working_dir, target_dir]) == absolute_working_dir
     if not valid_target_dir:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(target_dir):
+        return f'Error: "{directory}" is not a valid directory'
 
 get_files_info("calculator")
