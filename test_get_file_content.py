b276dae71c14ed18d45687757e3fe87a55219357
bmonagan <bernardmonagan@gmail.com>
2026-04-08 19:40:55 -0400

    fix: add print statements to output results of get_file_content tests

--- test_get_file_content.py
+++ test_get_file_content.py
@@ -1,17 +1,17 @@
 from functions.get_file_content import get_file_content
 
 print("Test: read existing file lorem.txt")
-get_file_content("calculator", "lorem.txt")
+print(get_file_content("calculator", "lorem.txt"))
 print("---")
 
 print("Test: read file in subdirectory pkg/__init__.py")
-get_file_content("calculator", "pkg/calculator.py")
+print(get_file_content("calculator", "pkg/calculator.py"))
 print("---")
 
 print("Test: attempt to read file outside working directory /bin/cat")
-get_file_content("calculator", "/bin/cat")
+print(get_file_content("calculator", "/bin/cat"))
 print("---")
 
 print("Test: read non-existent file pkg/does_not_exist.py")
-get_file_content("calculator", "pkg/does_not_exist.py")
+print(get_file_content("calculator", "pkg/does_not_exist.py"))
 print("---")
