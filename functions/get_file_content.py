1aa0aa0dd9f3a17a0af3b7b2826deff02584d329
bmonagan <bernardmonagan@gmail.com>
2026-04-08 19:40:27 -0400

    fix: improve error handling and remove print statements in get_file_content function

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -8,20 +8,17 @@ def get_file_content(working_directory, file_path):
     valid_target_dir = os.path.commonpath([absolute_working_dir, abs_file_path]) == absolute_working_dir
 
     if not valid_target_dir:
-        print(f'Error: Cannot read "{file_path}" as it is outside the permitted working directory')
-        return
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
     
-    print(abs_file_path)
     
     if not os.path.isfile(abs_file_path):
-        print(f'Error: File not found or is not a regular file: "{abs_file_path}"')
-        return
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+        
     try:
         with open(abs_file_path, 'r', encoding='utf-8') as file:
             content = file.read(MAX_CHARS)
             if file.read(1):
                 content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
-            print(content)
+            return content
     except Exception as e:
-        print(f'Error reading file "{file_path}": {e}')
-        return
+        return f'Error reading file "{file_path}": {e}'
