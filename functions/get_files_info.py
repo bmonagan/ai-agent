6ae5cccfcdd9d16ebd68f7042e6d75be4495b551
bmonagan <bernardmonagan@gmail.com>
2026-04-07 21:31:37 -0400

    refactor get_files_info function to improve directory validation and output formatting

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -10,6 +10,22 @@ def get_files_info(working_directory, directory="."):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     
     if not os.path.isdir(target_dir):
-        return f'Error: "{directory}" is not a valid directory'
+        return f'Error: "{directory}" is not a directory'
+
+    files_info = []
+    print(os.listdir(target_dir))
+    for entry in os.listdir(target_dir):
+        entry_path = os.path.join(target_dir, entry)
+        file_info = {
+            "name": entry,
+            "size": os.path.getsize(entry_path),
+            "is_directory": os.path.isdir(entry_path),
+        }
+        file_info_str = f"- {file_info['name']}: file_size={file_info['size']} bytes, is_dir{file_info['is_directory']}"
+        files_info.append(file_info_str)
+    print(f"Result for {directory if directory != "." else 'current directory'}")
+    for info in files_info:
+        print(info)
+        
 
 get_files_info("calculator")
