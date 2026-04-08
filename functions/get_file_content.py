6f81fe06fcdaef5947628b22f575e210494bc026
bmonagan <bernardmonagan@gmail.com>
2026-04-08 19:13:20 -0400

    feat: implement get_file_content function with validation checks

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,2 +1,14 @@
+import os 
+
+
 def get_file_content(working_directory, file_path):
-    pass
+    valid_target_dir = os.path.commonpath([working_directory, file_path]) == working_directory
+
+    if not valid_target_dir:
+        print(f'Error: Cannot read "{file_path}" as it is outside the permitted working directory')
+        return
+    
+    if not os.path.isfile(file_path):
+        print(f'Error: File not found or is not a regular file: "{file_path}"')
+        return
+    
