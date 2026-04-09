3f40afab39b77aff4f9fb2faf682aefd9cf94e74
bmonagan <bernardmonagan@gmail.com>
2026-04-08 23:02:34 -0400

    refactor: reorganize error handling and command execution in run_python_file function

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -2,24 +2,36 @@ import os
 import subprocess
 from functions.verify_file_path import verify_file_path
 def run_python_file(working_directory, file_path, args=None):
-    if not verify_file_path(working_directory, file_path):
-        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
-    if not os.path.isfile(file_path):
-        return f'Error: "{file_path}" does not exist or is not a regular file'
-    if not file_path.endswith('.py'):
-        return f'Error: "{file_path}" is not a Python file'
-    absolute_file_path = os.path.normpath(os.path.join(os.path.abspath(working_directory), file_path))
-    command = ["python", absolute_file_path]
-    if args:
-        command.extend(args)
-
     try:
+        if not verify_file_path(working_directory, file_path):
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        absolute_file_path = os.path.normpath(os.path.join(os.path.abspath(working_directory),file_path)) 
+        if not os.path.isfile(absolute_file_path):
+            return f'Error: "{file_path}" does not exist or is not a regular file'
+        if not file_path.endswith('.py'):
+            return f'Error: "{file_path}" is not a Python file'
+        
+        command = ["python", absolute_file_path]
+        if args:
+            command.extend(args)
+
+    
         result = subprocess.run(command, capture_output=True, text=True, cwd=working_directory)
         if result.returncode != 0:
             outputError = f'"Process exited with code X: {result.returncode}'
             if not result.stdout.strip() and not result.stderr.strip():
                 outputError += "No output produced"
             return outputError
+        else:  
+            out = result.stdout.strip()
+            err = result.stderr.strip()
+            output = ""
+            if out:
+                output += f"Standard Output:\n{out}\n"
+            if err:
+                output += f"Standard Error:\n{err}\n"
+            return output   
+            
     except Exception as e:
         return f"Error: executing Python file: {e}"
     
