514bb2c2410e35d440060129d79243910a4f0c5b
bmonagan <bernardmonagan@gmail.com>
2026-04-10 22:32:12 -0400

    feat: add missing function imports and update available functions dictionary

--- functions/call_function.py
+++ functions/call_function.py
@@ -1,7 +1,11 @@
 from google.genai import types
+from functions.get_files_info import get_files_info
 from functions.get_files_info import schema_get_files_info
+from functions.get_file_content import get_file_content
 from functions.get_file_content import schema_get_file_content
+from functions.run_python_file import run_python_file
 from functions.run_python_file import schema_run_python_file
+from functions.write_file import write_file
 from functions.write_file import schema_write_file
 
 
@@ -13,3 +17,16 @@ available_functions = types.Tool(
         schema_write_file,
     ],
 )
+
+available_functions_dict = {
+    "get_files_info": get_files_info,
+    "get_file_content": get_file_content,
+    "run_python_file": run_python_file,
+    "write_file": write_file,
+}
+
+def call_function(function_call: types.FunctionCall, verbose=False, working_directory="."):
+    if verbose:
+        print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(f" - Calling function: {function_call.name}")
